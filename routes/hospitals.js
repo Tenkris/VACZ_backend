fb88f1632c2a71b917c8b9ceea670e038f0433bd
Krittapas Rungsimontuchat <nineten609@gmail.com>
2023-12-17 13:29:05 +0700

    feat:prettierrc

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,9 +1,19 @@
-const express = require('express'); 
-const router = express.Router(); 
+const express = require('express');
+const router = express.Router();
 
-const {getHospitals , getHospital, createHospital, updateHospital,deleteHospital}  = require('../controllers/hospotals');
+const {
+  getHospitals,
+  getHospital,
+  createHospital,
+  updateHospital,
+  deleteHospital,
+} = require('../controllers/hospotals');
 
 router.route('/').get(getHospitals).post(createHospital);
-router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
+router
+  .route('/:id')
+  .get(getHospital)
+  .put(updateHospital)
+  .delete(deleteHospital);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,19 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config/config.env' });
-// body parser 
+// body parser
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const hospitals = require('./routes/hospitals');
- 
-app.use('/api/v1/hospitals', hospitals); 
+
+app.use('/api/v1/hospitals', hospitals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/` , process.env.NODE_ENV);
+  console.log(
+    `Server running at http://localhost:${port}/`,
+    process.env.NODE_ENV
+  );
 });
-
-
