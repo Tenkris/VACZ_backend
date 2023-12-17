572e49c31a90d0ca04f888cbc35e1c64baaf30c9
Krittapas Rungsimontuchat <nineten609@gmail.com>
2023-12-17 12:58:12 +0700

    feat: hospital crud rounter and controller

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config/config.env' });
-
+// body parser 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const hospitals = require('./routes/hospitals');
+ 
+app.use('/api/v1/hospitals', hospitals); 
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
-
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at http://localhost:${port}/` , process.env.NODE_ENV);
 });
 
 
